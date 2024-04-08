1037ba5a8627e9f21cad051424785712c3a492f3
Nitish Nashine <nitishnashine@gmail.com>
2024-04-08 18:49:09 +0530

    user model done

---
+++ server/src/models/user.model.js
@@ -0,0 +1,55 @@
+import mongoose, { Schema } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+      index: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+    },
+    fullname: {
+      type: String,
+      required: true,
+      trim: true,
+      index: true,
+    },
+    avatar: {
+      type: String, //cloudinary url
+      default: "https://www.gravatar.com/avatar/?d=mp",
+      required: true,
+    },
+    coverImage: {
+      type: String, //cloudinary url
+      default: "https://www.gravatar.com/avatar/?d=mp",
+      required: true,
+    },
+    watchHistory: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Video",
+      },
+    ],
+    password: {
+      type: String,
+      required: true,
+    },
+    refreshToken: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const User = mongoose.model("User", userSchema);
